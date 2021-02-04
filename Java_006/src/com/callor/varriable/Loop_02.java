c8603b9d44b718b38e63590582bb49f9f5d9e008
dust1234568 <ehdgml3526@gmail.com>
2021-02-04 14:41:29 +0900

    2.4-5

---
+++ Java_006/src/com/callor/varriable/Loop_02.java
@@ -0,0 +1,15 @@
+package com.callor.varriable;
+
+public class Loop_02 {
+	public static void main(String[] args) {
+		
+		int num1 = 0;
+		for ( ;; ) {
+			
+			num1 += 1;
+			System.out.println(num1);
+		}
+		
+		
+	}
+}

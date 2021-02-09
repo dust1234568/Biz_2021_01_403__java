af3343c77ff3d854060f743bf43639fbe8c2c859
dust1234568 <ehdgml3526@gmail.com>
2021-02-09 14:12:53 +0900

    2.9-6

---
+++ Java_009/src/com/callor/start/jdk/Random_06.java
@@ -0,0 +1,26 @@
+package com.callor.start.jdk;
+
+import java.util.Random;
+
+public class Random_06 {
+
+	public static void main(String[] args) {
+
+		Random rnd = new Random();
+
+		for (int i = 0; i < 100; i++) {
+			int ranNum = rnd.nextInt(100) + 1;
+			System.out.println(ranNum);
+
+			boolean bYes = ranNum > 34;
+			if (bYes) {
+				System.out.println((i + 1) + " 번째에서 나타남"); // 체크
+
+				break;
+			}
+
+		}
+
+	}
+
+}

07504df2f4e7a5a4947d9c6116d8adcb8ae964ad
dust1234568 <ehdgml3526@gmail.com>
2021-02-17 14:07:32 +0900

    method

---
+++ Java_011/src/com/callor/applications/Score_04.java
@@ -0,0 +1,26 @@
+package com.callor.applications;
+
+import com.callor.applications.service.ScoreServiceV3;
+
+public class Score_04 {
+
+	public static void main(String[] args) {
+
+		ScoreServiceV3 scV3 = new ScoreServiceV3();
+		// ScoreServiceV3 클래스의 2번째 sum() method 호출
+		scV3.sum();
+		// ScoreServiceV3 클래스의 1번째 sum(int num1, int num2) 호출
+		scV3.sum(20, 40);
+
+		float fnum1 = 43.25f;
+		float fnum2 = 32.11f;
+		// ScoreServiceV3 클래스의 3번째 sum(float f1, float f2) 호출
+		// 실수형변수 fnum1과 fnum2에 담긴 값을 sum() method에 전달하면서 호출
+		scV3.sum(fnum1, fnum2);
+
+		scV3.sum(100D, fnum1);
+		scV3.sum((double) 100, fnum1);
+
+	}
+
+}
---
+++ Java_011/src/com/callor/applications/service/ScoreServiceV4.java
@@ -0,0 +1,16 @@
+package com.callor.applications.service;
+
+public class ScoreServiceV4 {
+
+//	public void score(int intSum) {
+//		System.out.print("총점 : ");
+//		System.out.println(intSum);
+//	}
+//	public void score(float floatAvg) {
+//		System.out.print("평균 : ");
+//		System.out.println(floatAvg);
+//	}
+	public void score(int intSum, float floatAvg) {
+		System.out.print("촣점 : " + intSum + "\n" + "평균 : " + floatAvg);
+	}
+}

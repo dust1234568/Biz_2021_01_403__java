ca4895d76ca0e325bb5193c1d3d4436728cb3074
dust1234568 <ehdgml3526@gmail.com>
2021-02-17 11:51:49 +0900

    args

--- Java_011/src/com/callor/applications/service/ScoreService.java
+++ Java_011/src/com/callor/applications/service/ScoreService.java
@@ -2,14 +2,31 @@ package com.callor.applications.service;
 
 import java.util.Scanner;
 
+/*
+ * java의 class와 method 호출
+ * ScoreService 클래스에는 main() method가 없어서
+ * 여기에 있는 코드는 직접 Run(^F11)하여 실행할 수 없다
+ * 누군가가 ScoreService를 객체, 인스턴스로 만들고
+ * score() method를 호출해주어야만 코드를 실행할 수 있다
+ * 
+ * 다른 언어에서는 module 등의 이름으로 사용한다
+ * 
+ */
 public class ScoreService {
 
+	/*
+	 * 클래스와 메서드(메소드, method)의 명명법
+	 * 클래스 : 첫 글자 영문 대문자, 이후 영문 대소문자 숫자
+	 * 메서드 : 첫 글자 영문 소문자, 이후 영문 대소문자 숫자
+	 * 
+	 * 클래스 명명법 (Upper) CamelCase 라고 한다
+	 * 메서드 명명법 (Lower) CamelCase 라고 한다
+	 */
 	public void Score() {
 
 		Scanner Scan = new Scanner(System.in);
 
 		System.out.print("국어 점수를 입력하세요 >> ");
-
 		int intKor = Scan.nextInt();
 
 		System.out.print("영어 점수를 입력하세요 >> ");

ed41476af6e8bfd3cb304796795b997c9af0fe88
dust1234568 <ehdgml3526@gmail.com>
2021-02-25 09:55:46 +0900

    Product_01

---
+++ Java_017_oop/src/com/callor/oop/Product_01.java
@@ -0,0 +1,37 @@
+package com.callor.oop;
+
+import java.util.Scanner;
+
+import com.callor.oop.model.ProductVO;
+
+public class Product_01 {
+
+	public static void main(String[] args) {
+
+		ProductVO pVO = new ProductVO();
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("상품 코드 입력 >> ");
+		pVO.strPCode = scan.nextLine();
+
+		System.out.print("상품명 입력 >> ");
+		pVO.strPName = scan.nextLine();
+
+		System.out.print("품목 입력 >> ");
+		pVO.strItem = scan.nextLine();
+
+		System.out.print("거래처 입력 >> ");
+		pVO.strDName = scan.nextLine();
+
+		System.out.print("매입 단가 입력 >> ");
+		pVO.iPrice = scan.nextInt();
+
+		System.out.print("매출 단가 입력 >> ");
+		pVO.oPrice = scan.nextInt();
+
+		System.out.println("=======================================================");
+		System.out.printf("%s\t%s\t%s\t%s\t%d\t%d\n", pVO.strPCode, pVO.strPName, pVO.strItem, pVO.strDName, pVO.iPrice,
+				pVO.oPrice);
+		System.out.println("-------------------------------------------------------");
+	}
+}
---
+++ Java_017_oop/src/com/callor/oop/model/ProductVO_01.java
@@ -0,0 +1,17 @@
+package com.callor.oop.model;
+
+public class ProductVO_01 {
+
+	// 상품관리
+	public String strCode;		// 상품코드
+	public String strName;		// 상품명
+	public String strSub;		// 품목명
+	public String strCompany;	// 거래처
+	public int intInPrice;		// 매입단가
+	public int intOutPric;		// 매출단가
+
+	// 상품거래
+	public int intPrice;		// 단가
+	public int intCount;		// 수량
+
+}

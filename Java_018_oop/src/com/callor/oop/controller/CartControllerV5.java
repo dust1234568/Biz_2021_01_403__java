15548c1c99a4393254d698bdc713f0ddfa61d190
dust1234568 <ehdgml3526@gmail.com>
2021-02-26 17:29:02 +0900

    2.26_1

--- Java_018_oop/src/com/callor/oop/controller/CartControllerV5.java
+++ Java_018_oop/src/com/callor/oop/controller/CartControllerV5.java
@@ -7,9 +7,9 @@ public class CartControllerV5 {
 	public static void main(String[] args) {
 
 		CartServiceV1 csV1 = new CartServiceV1();
-		
+
 		csV1.inputCart();
-		
+
 		csV1.printCartList();
 
 	}
--- Java_018_oop/src/com/callor/oop/service/CartServiceV1.java
+++ Java_018_oop/src/com/callor/oop/service/CartServiceV1.java
@@ -13,6 +13,7 @@ public class CartServiceV1 {
 	Scanner scan = new Scanner(System.in);
 
 	public void inputCart() {
+
 		for (int i = 0; i < 3; i++) {
 
 			System.out.println("======================================");
@@ -60,6 +61,7 @@ public class CartServiceV1 {
 			intSum = intQty * intPrice;
 
 			CartVO csV1 = new CartVO();
+
 			csV1.setCartUserName(strUserName);
 			csV1.setCartPname(strPName);
 			csV1.setCartQty(intQty);
@@ -68,21 +70,22 @@ public class CartServiceV1 {
 
 			cartList.add(csV1);
 
-		} // end for
+		}
 
 	}
+
 	public void printCartList() {
-		
-		System.out.println("=======================================");
+
+		System.out.println("============================================");
 		System.out.println("구매자\t상품명\t수량\t단가\t총계");
-		System.out.println("---------------------------------------");
+		System.out.println("--------------------------------------------");
 
 		int nSize = cartList.size();
 		for (int i = 0; i < nSize; i++) {
 			System.out.printf("%s\t%s\t%d\t%d\t%d\n", cartList.get(i).getCartUserName(), cartList.get(i).getCartPname(),
 					cartList.get(i).getCartQty(), cartList.get(i).getCartPrice(), cartList.get(i).getCartTotal());
 		}
-		
+
 	}
 
 }

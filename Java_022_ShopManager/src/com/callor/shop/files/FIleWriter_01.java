8829d8adb40a46b6cea4350087bd673f7d5fbe3f
dust1234568 <ehdgml3526@gmail.com>
2021-03-05 10:49:02 +0900

    filewriter

--- Java_022_ShopManager/src/com/callor/shop/files/FIleWriter_01.java
+++ Java_022_ShopManager/src/com/callor/shop/files/FIleWriter_01.java
@@ -1,4 +1,4 @@
-package com.callor.shop;
+package com.callor.shop.files;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,8 +13,8 @@ public class FIleWriter_01 {
 		Random rnd = new Random();
 		// 100개의 임의의 난수를 생성하기 위해 배열 선언
 		int[] nums = new int[100];
-		
-		String numsFileName = "src/com/callor/shop/nums.txt";
+
+		String numsFileName = "src/com/callor/shop/files/nums.txt";
 
 		FileWriter fileWriter = null;
 		PrintWriter printer = null;
@@ -22,11 +22,11 @@ public class FIleWriter_01 {
 		try {
 			fileWriter = new FileWriter(numsFileName);
 			printer = new PrintWriter(fileWriter);
-			for (int i = 0; i < 100; i++) {
+			for (int i = 0; i < nums.length; i++) {
 				nums[i] = rnd.nextInt(1000) + 1;
 				System.out.println(nums[i]);
 				// 숫자형으로 형변환 하기위해 문자열로 입력시 사용
-				// nums[i] = Integer.valueOf(nums[i]); 
+				nums[i] = Integer.valueOf(nums[i]);
 			}
 
 			for (int i = 0; i < nums.length; i++) {
